a20767b547539c9ae5538e7079ce832d931f7090
k <ooggie118@gmail.com>
2025-07-11 11:35:56 -0400

    Update payment.routes.ts

--- FERREMAS-masternew/src/routes/payment.routes.ts
+++ FERREMAS-masternew/src/routes/payment.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { createOrder, success,  failure, pending, webhook} from "../controller/payment.controller";
+import { createOrder, webhook} from "../controller/payment.controller";
 
 const paymentRoutes = Router();
 
 paymentRoutes.post("/create", createOrder);
-paymentRoutes.get("/success", success);
-paymentRoutes.get("/failure", failure);
-paymentRoutes.get("/pending", pending);
+
 
 paymentRoutes.post("/webhook", webhook);
 

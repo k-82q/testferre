20938b79d54d67ad62eb26bb1a9623270578dc65
k <ooggie118@gmail.com>
2025-07-11 10:36:37 -0400

    serverr

--- FERREMAS-masternew/src/server.ts
+++ FERREMAS-masternew/src/server.ts
@@ -19,7 +19,8 @@ export class Server{
     };
     constructor(){
         this.app = express();
-        this.port = process.env.PORT || '3000';
+        const port = process.env.PORT || 3000;
+        this.port = port.toString();
         this.middlewares();
         this.routes();
     }
